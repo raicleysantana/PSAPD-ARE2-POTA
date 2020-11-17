5f21297bf59f9ba55c793ba3b6e15662f2466ce6
Raicley Santana <raicleysantana1@gmail.com>
2020-11-17 19:51:17 -0400

    Adicionado metodos

--- main.c
+++ main.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//I - Estrutura de Dados Básica
+	
 struct Alunos{
 	char matricula;
 	char nome_completo;
 	char curso;
 	float media_geral_final;
-};
+} alunos[5];	
 
+void pesquisa_linear_nome(char nome);
+void pesquisa_linear_matricula(char matricula);
+void pesquisa_binaria_nome(char nome);
+void pesquisa_binaria_matricula(char matricula);
 
+void listagem_por_insercao();
+void listagem_por_selecao();
+void listagem_por_quickSort();
 
 int main(int argc, char *argv[]) {
 	setlocale (LC_ALL, "portuguese");
@@ -18,15 +27,19 @@ int main(int argc, char *argv[]) {
 
 	do{
 		system("cls");
-		printf("\n1 <- Cadastrar aluno");
-		printf("\n2 <- Pesquisar aluno");
-		printf("\n3 <- Listar aluno");
-		printf("\n0 <- sair");
+		printf("\n\t 1 <- Cadastrar aluno");
+		printf("\n\t 2 <- Pesquisar aluno");
+		printf("\n\t 3 <- Listar aluno");
+		printf("\n\t 0 <- sair");
+		printf("\n");
 		
 		printf("\nSelecione uma opção: ");
 		scanf("%i",&opcao);
 		
 		switch(opcao){
+			case 0 :
+				exit(1);
+				break;
 			case 1 :
 				break;
 			case 2:
@@ -38,12 +51,13 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 		
-	}while(opcao != 0);
+	}while(opcao != 4);
 	
 	return 0;
 }
 
 
+//II - Modos de Pesquisa
 
 void pesquisa_linear_nome(char nome){
 	
@@ -58,5 +72,20 @@ void pesquisa_binaria_nome(char nome){
 }
 
 void pesquisa_binaria_matricula(char matricula){
+	
+}
+
+
+//III – Métodos de Ordenação
+
+void listagem_por_insercao(){
+	
+}
+
+void listagem_por_selecao(){
+	
+}
+
+void listagem_por_quickSort(){
 	
 }
